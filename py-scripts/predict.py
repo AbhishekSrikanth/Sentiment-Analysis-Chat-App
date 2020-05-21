8be5ce3efc83d2ff1138b76d61cf4ad4daeb2e34
Abhishek Srikanth <abhisrik99@outlook.com>
2020-05-21 17:18:40 +0530

    Clean up demo script

--- py-scripts/predict.py
+++ py-scripts/predict.py
@@ -3,10 +3,6 @@ import sys
 from textblob import TextBlob
 text = sys.argv[1]
 
-# import numpy as np
-# num = np.random.randint(0,2)
-# print(num)
-
 polarity = TextBlob(text).sentiment.polarity
 if polarity > 0.0:
     print("success")
